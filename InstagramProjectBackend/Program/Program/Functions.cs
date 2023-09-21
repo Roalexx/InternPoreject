4957d016b3b0ee254ab26186287461e3126db5da
Roalexx <ahmetdmrlceng@gmail.com>
2023-09-21 11:43:01 +0300

    try

---
+++ InstagramProjectBackend/Program/Program/Functions.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace BackendApp
+{
+    public class Functions
+    {  
+        public static  async Task<JObject> AsyncHttpRequest(string link, string token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                // HTTP Headers
+                client.DefaultRequestHeaders.Add("cookie", "mid=ZMtaqQALAAFIsrg55_kPJsY-hPgb; ig_did=89F1C663-68C0-417A-834B-B87841E39AF2; ig_nrcb=1; datr=qFrLZIMGM-iWwZy7pkG_ltkN; fbm_124024574287414=base_domain=.instagram.com; csrftoken=zE7SdCdd8Wf14jzxdExgVmzCXrk9LScL; ds_user_id=8240144112; sessionid=8240144112:6phchbPPsa4SSz:20:AYe8jjamDsa27Ck7PbcdxgRO6J15-akw0nPYaw9j4g; shbid=3313; shbts=1695131722; fbsr_124024574287414=ddDb2DnUUgI5AfeKuuEqVGnLMrVtmxdKzmvlezBx0CI.eyJ1c2VyX2lkIjoiMTAwMDA4NDY8HHHHHHHHHHHHHHH");
+                client.DefaultRequestHeaders.Add("x-ig-app-id", token);  // Değişkeni burada kullanıyoruz
+
+                HttpResponseMessage response = await client.GetAsync(link);
+                JObject jsonObject;
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+                    jsonObject = JObject.Parse(content);
+                }
+                else
+                {
+                    Console.WriteLine("İstek başarısız oldu. HTTP kodu: " + response.StatusCode);
+                    jsonObject = null;
+                }
+                return jsonObject;
+            }
+        }
+
+        public static List<string> CompareFollows(InstagramResponse response1, InstagramResponse response2)
+        {
+            HashSet<string> followers1 = new HashSet<string>();
+            HashSet<string> followers2 = new HashSet<string>();
+
+            foreach (var user in response1.Users)
+            {
+                followers1.Add(user.Username);
+            }
+            foreach (var user in response2.Users)
+            {
+                followers2.Add(user.Username);
+            }
+            followers1.IntersectWith(followers2);
+
+            List<string> commonFollowers = followers1.ToList();            
+            return commonFollowers;
+        }
+    }
+}
---
+++ InstagramProjectBackend/Program/Program/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using BackendApp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+class Program
+{
+    static async Task Main()
+    {   //iqAppId kullanıcı tokenidir yani kimi görmek istediğin değil kimin görmek istediğidir
+        string igAppId = "1754362624997642";
+        //user id hangi kullanıcının takip ettiklerini görmek isrtiyorsan onlar
+        string target1UserName = "leomessi";
+        string target2UserName = "cristiano";
+        //kullanıcı id si alma
+        string idCallerLink = "https://www.instagram.com/api/v1/users/web_profile_info/?username=" + target1UserName;
+
+        //userid call user follower and following count for target1
+        JObject dataInfo = await Functions.AsyncHttpRequest(idCallerLink, igAppId);
+        string target1UserId = dataInfo["data"]["user"]["id"].ToString(); //ilk kullanıcının user id si
+        long target1FollowerCount = (long)((Newtonsoft.Json.Linq.JValue)dataInfo["data"]["user"]["edge_followed_by"]["count"]).Value; //ilk kullanıcın takipçi sayısı
+        long target1FollowingCount = (long)((Newtonsoft.Json.Linq.JValue)dataInfo["data"]["user"]["edge_follow"]["count"]).Value; //ilk kullanıcının takp ettiği kişi sayısı
+
+        //userid call user follower and following count for target2
+        idCallerLink = "https://www.instagram.com/api/v1/users/web_profile_info/?username=" + target2UserName;
+        dataInfo = await Functions.AsyncHttpRequest(idCallerLink, igAppId);
+        string target2UserId = dataInfo["data"]["user"]["id"].ToString(); //ikinci kullanıcının user id si
+        long target2FollowerCount = (long)((Newtonsoft.Json.Linq.JValue)dataInfo["data"]["user"]["edge_followed_by"]["count"]).Value; // ikinci kullanıcın takipçi sayısı
+        long target2FollowingCount = (long)((Newtonsoft.Json.Linq.JValue)dataInfo["data"]["user"]["edge_follow"]["count"]).Value; //ilk kullanıcının takp ettiği kişi sayısı
+
+        //user 1 in takip ettiklerini alalım
+        long max_id = 0;
+        bool whileController = true;
+        string followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target1UserId + "/following/?count=200&max_id=" + max_id;
+        string data1;
+        InstagramResponse response1;
+        //response1 e onceki verileri ekleyemiyouz sadece son veriler yazılıyo onu züzelt
+        while (whileController)
+        {
+            if (target1FollowerCount > max_id)
+            {
+                followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target1UserId + "/following/?count=200&max_id=" + max_id;
+                dataInfo = await Functions.AsyncHttpRequest(followingCallerLink, igAppId);
+                data1 = dataInfo.ToString();
+                response1 = JsonConvert.DeserializeObject<InstagramResponse>(data1);
+                max_id += 200;
+            }
+            else
+            {
+                whileController = false;
+            }
+        }
+
+        //user 2 nin takiip ettiklerini alalım
+        max_id = 0;
+        whileController = true;     
+        followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target2UserId + "/following/?count=200&max_id="+max_id;
+        while (whileController)
+        {
+            if (target1FollowerCount > max_id)
+            {
+                followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target2UserId + "/following/?count=200&max_id=" + max_id;
+                dataInfo = await Functions.AsyncHttpRequest(followingCallerLink, igAppId);
+                max_id += 200;
+            }
+            else
+            {
+                whileController = false;
+            }
+        }
+        string data2 = dataInfo.ToString();
+        
+        InstagramResponse response2 = JsonConvert.DeserializeObject<InstagramResponse>(data2);
+
+        //user 1 ve user 2 nin ortak olan takip ettiklerini alalım
+        //List<string> commonFollowing = Functions.CompareFollows(response1, response2);
+        //foreach (var followings in commonFollowing)
+        //{
+        //    await Console.Out.WriteLineAsync(followings);
+        //}
+    }
+}
