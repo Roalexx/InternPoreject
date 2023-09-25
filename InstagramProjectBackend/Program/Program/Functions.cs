4cf3c23770e3318f787bc04cc42740909fcf2ae4
Roalexx <ahmetdmrlceng@gmail.com>
2023-09-25 11:59:59 +0300

    Final Version

--- InstagramProjectBackend/Program/Program/Functions.cs
+++ InstagramProjectBackend/Program/Program/Functions.cs
@@ -10,19 +10,19 @@ using System.Threading.Tasks;
 namespace BackendApp
 {
     public class Functions
-    {  
-        public static  async Task<JObject> AsyncHttpRequest(string link, string token, string cookie)
+    {
+        public static async Task<JObject> AsyncHttpRequest(string link, string token, string cookie)
         {
             using (HttpClient client = new HttpClient())
             {
                 // HTTP Headers
                 client.DefaultRequestHeaders.Add("cookie", cookie);
-                client.DefaultRequestHeaders.Add("x-ig-app-id", token);  
+                client.DefaultRequestHeaders.Add("x-ig-app-id", token);
 
                 HttpResponseMessage response = await client.GetAsync(link);
                 JObject jsonObject;
                 string content = await response.Content.ReadAsStringAsync();
-                jsonObject = JObject.Parse(content);        
+                jsonObject = JObject.Parse(content);
                 return jsonObject;
             }
         }
@@ -42,14 +42,14 @@ namespace BackendApp
             }
             followers1.IntersectWith(followers2);
 
-            List<string> commonFollowers = followers1.ToList();            
+            List<string> commonFollowers = followers1.ToList();
             return commonFollowers;
         }
-        public static async void RequestCaller(string link,string token, string cookie, long count)
+        public static async Task<JObject> RequestCaller(string link, string token, string cookie, long count)
         {
-            string deneme;
-            string url = link + "/following/?count=200&max_id=" + 0;
-            JObject dataInfo;
+            string url = link + 0;
+            JObject result = new JObject();
+
             if (count > 1200)
             {
                 Console.WriteLine("Takip edilen yada takipçi sayısı çok fazla");
@@ -58,20 +58,22 @@ namespace BackendApp
             {
                 long counter = count / 200;
                 long mod = count % 200;
-                for (int i = 0; i < count; i += 200)
+
+                for (int i = 0; i < counter; i++)
                 {
-                   dataInfo = await  AsyncHttpRequest(url, token, cookie);
-                    deneme = dataInfo.ToString();
-                    url=link+ "/following/?count=200&max_id=" + i; 
-                    await Console.Out.WriteLineAsync(deneme);
+                    url = link + "/following/?count=200&max_id=" + (i * 200);
+                    JObject dataInfo = await AsyncHttpRequest(url, token, cookie);
+                    result.Merge(dataInfo); // dataInfo'yu result ile birleştir
                 }
+
                 url = link + "/following/?count=200&max_id=" + (count - mod);
-                dataInfo = await AsyncHttpRequest(url, token, cookie);
-                deneme = dataInfo.ToString();
-                await Console.Out.WriteLineAsync(deneme);
+                JObject lastDataInfo = await AsyncHttpRequest(url, token, cookie);
+                result.Merge(lastDataInfo); // Son datayı da result ile birleştir
             }
 
+            return result;
         }
+
 
     }
 }
--- InstagramProjectBackend/Program/Program/Program.cs
+++ InstagramProjectBackend/Program/Program/Program.cs
@@ -10,8 +10,9 @@ class Program
 {
     static async Task Main()
     {   //iqAppId kullanıcı tokenidir yani kimi görmek istediğin değil kimin görmek istediğidir
-        string igAppId = "936619743392459";
+        string igAppId = ""; //x-ig-app-id inizi buraya girin
         //user id hangi kullanıcının takip ettiklerini görmek isrtiyorsan onlar
+        await Console.Out.WriteLineAsync("Ortak takipleri görmek istediğiniz kullanıcı adını buraya girin");
         string target1UserName = Console.ReadLine();
         string target2UserName = Console.ReadLine();
         Console.Clear();
@@ -28,35 +29,34 @@ class Program
 
         //userid call user follower and following count for target2
         idCallerLink = "https://www.instagram.com/api/v1/users/web_profile_info/?username=" + target2UserName;
-        dataInfo = await Functions.AsyncHttpRequest(idCallerLink, igAppId,cookie);
+        dataInfo = await Functions.AsyncHttpRequest(idCallerLink, igAppId, cookie);
         string target2UserId = dataInfo["data"]["user"]["id"].ToString(); //ikinci kullanıcının user id si
         long target2FollowerCount = (long)((Newtonsoft.Json.Linq.JValue)dataInfo["data"]["user"]["edge_followed_by"]["count"]).Value; // ikinci kullanıcın takipçi sayısı
         long target2FollowingCount = (long)((Newtonsoft.Json.Linq.JValue)dataInfo["data"]["user"]["edge_follow"]["count"]).Value; //ilk kullanıcının takp ettiği kişi sayısı
 
         //user 1 in takip ettiklerini alalım
-        string followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target1UserId + "/following/?count=200&max_id=0";
+        string followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target1UserId + "/following/?count=200&max_id=";
         string data;
         cookie = "mid=ZMtaqQALAAFIsrg55_kPJsY-hPgb; ig_did=89F1C663-68C0-417A-834B-B87841E39AF2; ig_nrcb=1; datr=qFrLZIMGM-iWwZy7pkG_ltkN; fbm_124024574287414=base_domain=.instagram.com; shbid=\"3313\\0548240144112\\0541726667722:01f7b7962ae18830a8cd79434bc2654a249ea191151896080501165d678128ca001b723c\"; shbts=\"1695131722\\0548240144112\\0541726667722:01f72b2788590406f206b9357279582402e6fc3ffb5998442d33d029242a58274f00e0b6\"; ds_user_id=8240144112; sessionid=8240144112%3A92tWtdlPrUfRDw%3A7%3AAYdAQ793Grj7SZ_AStSlvkxoWfSWRKCTYhdkcO20ww; csrftoken=XMoipA7se4FGXCMwLChgZnQlSqitaQrr; fbsr_124024574287414=iik9aTPSeRoyfHEPb2uFJgO66cBqrkVZBpT8S8nu48w.eyJ1c2VyX2lkIjoiMTAwMDA4NDY4MzE3NjM1IiwiY29kZSI6IkFRRHd3QlZFem5uQ29Sc29sR1V6dUdGcGhMWlJpM3laeVg1NG5LSFVhNk9fckhNZXdtNXRoTzJNVjdYOHNfLUQtRHRvdzVhcjNULTdCUVY3bFd6ZUhUVVhMNmtZeFZ2SThGUU5xVFJ1Mkd3Q0tVQlNwaEFpWUJibWE3am5YQURGWktKMDFuNFRJbVByeFN5a2pWa1pSdkxKeU5ybVdLeFlkUU5lVkJkcG5Va3d5bVJqOGJETUY3RktZVVQ4U1ltZ1ZMZWdyVnhQT21fS1Z0NjJuZEtFWV93Q1pFdEpydmotQ3dVM3FHQjZTYVhBUGxHTkdFbGM2RFRYYTRmMl9yRmExd2JoMlVUNGdTd0xoamRJVERmMjN6V1Zwd1FMdHVySG9kUzlWcGFHUkhodFFXUDFVODc2N29jYTNUVGRjb2d4R1ZiRkMwbUdWX25UdzlrdEFiaE5WaUxsIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCTzQ5T3VhMkJUTTBFVmRYQlpBMjhDRzE0SkZRbEZKdExISVBFVzMzVzBKT1N4VXVXTGpCS0RWczF1bnM0WkFtV2cxZ0JVT3VPZTBRRFZ5TFpBcmxyVDhsVldjTU50WkMwVlpBdTJRU1Q5WEw2ME5yOUxSTVk0dlRXazVqTDJPY3FJVVlmQ3hNUUt4OEtSNTFtUm1QWG5aQm1BMjB6YlpDTFRvd1pBUldWRlR4ZFl6YTdIMDhCWWxtdnBYVVpEIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE2OTUzNzIwNjR9; fbsr_124024574287414=iik9aTPSeRoyfHEPb2uFJgO66cBqrkVZBpT8S8nu48w.eyJ1c2VyX2lkIjoiMTAwMDA4NDY4MzE3NjM1IiwiY29kZSI6IkFRRHd3QlZFem5uQ29Sc29sR1V6dUdGcGhMWlJpM3laeVg1NG5LSFVhNk9fckhNZXdtNXRoTzJNVjdYOHNfLUQtRHRvdzVhcjNULTdCUVY3bFd6ZUhUVVhMNmtZeFZ2SThGUU5xVFJ1Mkd3Q0tVQlNwaEFpWUJibWE3am5YQURGWktKMDFuNFRJbVByeFN5a2pWa1pSdkxKeU5ybVdLeFlkUU5lVkJkcG5Va3d5bVJqOGJETUY3RktZVVQ4U1ltZ1ZMZWdyVnhQT21fS1Z0NjJuZEtFWV93Q1pFdEpydmotQ3dVM3FHQjZTYVhBUGxHTkdFbGM2RFRYYTRmMl9yRmExd2JoMlVUNGdTd0xoamRJVERmMjN6V1Zwd1FMdHVySG9kUzlWcGFHUkhodFFXUDFVODc2N29jYTNUVGRjb2d4R1ZiRkMwbUdWX25UdzlrdEFiaE5WaUxsIiwib2F1dGhfdG9rZW4iOiJFQUFCd3pMaXhuallCTzQ5T3VhMkJUTTBFVmRYQlpBMjhDRzE0SkZRbEZKdExISVBFVzMzVzBKT1N4VXVXTGpCS0RWczF1bnM0WkFtV2cxZ0JVT3VPZTBRRFZ5TFpBcmxyVDhsVldjTU50WkMwVlpBdTJRU1Q5WEw2ME5yOUxSTVk0dlRXazVqTDJPY3FJVVlmQ3hNUUt4OEtSNTFtUm1QWG5aQm1BMjB6YlpDTFRvd1pBUldWRlR4ZFl6YTdIMDhCWWxtdnBYVVpEIiwiYWxnb3JpdGhtIjoiSE1BQy1TSEEyNTYiLCJpc3N1ZWRfYXQiOjE2OTUzNzIwNjR9; rur=\"FRC\\0548240144112\\0541726908078:01f7e6939b218280eeb53185363910c24ca435500140247f6a852ec703d812c0265a7449\"";
-        InstagramResponse response1;
-        //response1 e onceki verileri ekleyemiyouz sadece son veriler yazılıyo onu züzelt
-        dataInfo = await Functions.AsyncHttpRequest(followingCallerLink, igAppId, cookie);
+        dataInfo = await Functions.RequestCaller(followingCallerLink, igAppId, cookie, target1FollowingCount);
         data = dataInfo.ToString();
-        response1 = JsonConvert.DeserializeObject<InstagramResponse>(data);
+        InstagramResponse response1 = JsonConvert.DeserializeObject<InstagramResponse>(data);
         //user 2 nin takiip ettiklerini alalım
-        followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target2UserId + "/following/?count=200&max_id=0";
-        dataInfo = await Functions.AsyncHttpRequest(followingCallerLink, igAppId, cookie);
-        data = dataInfo.ToString();        
+        followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target2UserId + "/following/?count=200&max_id=";
+        dataInfo = await Functions.RequestCaller(followingCallerLink, igAppId, cookie, target2FollowingCount);
+        data = dataInfo.ToString();
         InstagramResponse response2 = JsonConvert.DeserializeObject<InstagramResponse>(data);
 
         //user 1 in takipçilerini alalım
-        idCallerLink = "https://www.instagram.com/api/v1/friendships/" + target1UserId + "/followers/?count=200&max_id=0";
-        dataInfo = await Functions.AsyncHttpRequest(followingCallerLink, igAppId, cookie);
+        idCallerLink = "https://www.instagram.com/api/v1/friendships/" + target1UserId + "/followers/?count=200&max_id=";
+        dataInfo = await Functions.RequestCaller(followingCallerLink, igAppId, cookie, target1FollowerCount);
         data = dataInfo.ToString();
         InstagramResponse response3 = JsonConvert.DeserializeObject<InstagramResponse>(data);
 
+
         //user 2 nin takipçilerini alalım
-        followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target2UserId + "/followers/?count=200&max_id=0";
-        dataInfo = await Functions.AsyncHttpRequest(followingCallerLink, igAppId, cookie);
+        followingCallerLink = "https://www.instagram.com/api/v1/friendships/" + target2UserId + "/followers/?count=200&max_id=";
+        dataInfo = await Functions.RequestCaller(followingCallerLink, igAppId, cookie, target2FollowerCount);
         data = dataInfo.ToString();
         InstagramResponse response4 = JsonConvert.DeserializeObject<InstagramResponse>(data);
 
